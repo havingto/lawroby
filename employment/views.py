5b343d5d49d7daed877c58b0c8f8ae5c6ea59da0
havingto <aabodonyi@gmail.com>
2022-08-16 03:36:26 +1000

    HTML invisible space holders

--- employment/views.py
+++ employment/views.py
@@ -14,7 +14,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-## To activate tracking for testing, replace '# # print(' with '# print('.
+## To activate tracking for testing, replace '# print(' with 'print('.
 
 ## The relative locations of the output pdf files
 RELEVANT_LAW = str(BASE_DIR) + '\\employment\\legislation\\relevant_law\\'
@@ -54,23 +54,23 @@ def intro(request):
 
 def interview(request):
 
-    # print('URL = interview')
-    # print('G.key, G.val, G.case, G.resolve, G.tense')
-    # print(G.key, G.val, G.case, G.resolve, G.tense)
+    print('URL = interview')
+    print('G.key, G.val, G.case, G.resolve, G.tense')
+    print(G.key, G.val, G.case, G.resolve, G.tense)
 
     if G.key == "":
 # redirection came from the intro page, i.e. the session is just starting
 
         G.key = 'issue'
         G.val = '(default)'
-        # print('First assignment of the Pair:  '  + G.key + '-' + G.val)
+        print('First assignment of the Pair:  '  + G.key + '-' + G.val)
 
     else:
 ## redirection came from the interview page with the choice (new_value) 
 ## submitted by the user
 
         requested = request.POST 
-        # print('Requested POST: ' + str(requested))
+        print('Requested POST: ' + str(requested))
 
 ## Check whether the request came from terminating class after a back page
         if G.html == 'exit':
@@ -98,15 +98,15 @@ def interview(request):
             if list(requested)[1] == 'comment':
 ## The response came from comment.html, so G.val will take on new_value in Comment record
 
-                # print('Searching for comment: '+ G.key+'-'+G.val)
+                print('Searching for comment: '+ G.key+'-'+G.val)
                 comment = Comment.objects.get(key=G.key, value=G.val)
                 G.val = str(comment.new_value)
-                # print('The new_value from comment: '+G.val+' So the new Pair is '+G.key+'-'+G.val)
+                print('The new_value from comment: '+G.val+' So the new Pair is '+G.key+'-'+G.val)
             else:
 ## the request was a comment.html, but the response came from a different page 
 ## after back page from user
                 comment = Comment.objects.get( key=G.key, value=G.val )
-                # print(G.key + '-' + G.val + ' is in Comment')
+                print(G.key + '-' + G.val + ' is in Comment')
                 if G.tense == 'past':
                     comment_text = comment.comment_text_past
                 else:
@@ -132,7 +132,7 @@ def interview(request):
             G.answers.append(a)
             ch = Choice.objects.get(id=pick)
             G.val = str(ch.new_value)
-            # print('user choice has been evaluated. The Pair is ' + G.key + '-' + G.val)
+            print('user choice has been evaluated. The Pair is ' + G.key + '-' + G.val)
 
         else:
 ## Mismatch of question_id. The response came from a previous page after a back page
@@ -153,7 +153,7 @@ def interview(request):
     
     while True:
 ## This loop determines what happens with the new Pair
-        # print('New pair? ' + str(new_pair))
+        print('New pair? ' + str(new_pair))
 
         while new_pair:
 ## This loop checks whether the value of a new Pair is reassigned either with a condition
@@ -176,19 +176,19 @@ def interview(request):
 
             if dependency.count() == 0:
 ## if G.val is not in dependency           
-                # print(G.key + '-' + G.val + '   is not in Dependency')
+                print(G.key + '-' + G.val + '   is not in Dependency')
                 new_pair = False
 
             else:
 ## Pair is in Dependency, the corresponding references (if any) will be evaluated and 
 ## the value of G.val will change
-                # print(G.key + '-' + G.val + ' is in dependency. G.val will change.')
-                # print('dependency:' + str(dependency))
+                print(G.key + '-' + G.val + ' is in dependency. G.val will change.')
+                print('dependency:' + str(dependency))
 
                 for d in dependency:
 ## Looping through the relevant records in Dependency, we check whether ref_key 
 ## has been evaluated, and (if it has) whether its (final)value is identical with ref_val
-                    # print('looping:' + str(d))
+                    print('looping:' + str(d))
                     d_id=int(str(d))
                     d_record=Dependency.objects.get(id=d_id)
 ## Arrange the reference pair into a list
@@ -197,11 +197,11 @@ def interview(request):
                     if reference[0] == "":
 ## If there is no reference to evaluate, it is a simple reassign of value.
                         G.val = str(d_record.new_value)
-                        # print('G.val has been reassigned, the new Pair: ' + G.key +'-' + G.val)
+                        print('G.val has been reassigned, the new Pair: ' + G.key +'-' + G.val)
                         break
                     else:
 ## There is a reference to evaluate.                        
-                        # print('The reference is ' + str(reference))
+                        print('The reference is ' + str(reference))
                         if reference[0] in list(G.case):                          
 ## If the reference key is in Case, i.e. it has been evaluated
 
@@ -211,9 +211,9 @@ def interview(request):
 ## record, and a new_pair is established. We break out of this for loop, and continue 
 ## with checking the new Pair in Dependency, so we stay in the while loop 
 ## (new_pair stays TRUE)
-                                # print(reference[0] + ' is in Case, and its value is ' + G.case[reference[0]])
+                                print(reference[0] + ' is in Case, and its value is ' + G.case[reference[0]])
                                 G.val = str(d_record.new_value)
-                                # print('The reference is satisfied (pair is in Case), new G.val: ' + G.key +'-' + G.val)
+                                print('The reference is satisfied (pair is in Case), new G.val: ' + G.key +'-' + G.val)
                                 break
 
 ## Otherwise, i.e. if the referenced key is in G.case, i.e. it has already been evaluated,
@@ -225,13 +225,13 @@ def interview(request):
 ## If the reference key is not in Case, i.e. it has not been evaluated, then the current 
 ## Pair is appended to the stack G.resolve, we break out of this for loop,
 ## and the evaluation of the reference key begins with the value "(default)". 
-                            # print(reference[0] + '  reference key has not been evaluated')
+                            print(reference[0] + '  reference key has not been evaluated')
                             r = [G.key, G.val]
                             G.resolve.append(r)
-                            # print('Pair appended to G.resolve: ' + str(G.resolve))
+                            print('Pair appended to G.resolve: ' + str(G.resolve))
                             G.key = reference[0]
                             G.val = '(default)'
-                            # print('New pair: ' + G.key +'-' + G.val)
+                            print('New pair: ' + G.key +'-' + G.val)
 ## As we found a new key to evaluate, we break out of this for loop and check if it
 ## too has been reassigned in Dependency.
                             break
@@ -239,49 +239,49 @@ def interview(request):
 ## This is the end of the for loop, i.e. all of the reference pairs tried, but none
 ## matched. The Pair is unchanged, i.e. stops being a new pair.
 ## (It will be tried in the other classes.).
-                    # print('End of for loop of dependency records. None satisfied.')
+                    print('End of for loop of dependency records. None satisfied.')
                     new_pair = False       
         try:
             abort=Abort.objects.get(key=G.key, value=G.val)
         except ObjectDoesNotExist:
-            # print(G.key + '-' + G.val + '   is not to abort')
+            print(G.key + '-' + G.val + '   is not to abort')
             pass
         else:
 # aborted, likely due to jurisdiction problems
-            # print(G.key + '-' + G.val + ' is in Abort')
+            print(G.key + '-' + G.val + ' is in Abort')
             G.html = "abort"
             return render( request, 'employment/abort.html')
         try:
             other=Other.objects.get(key=G.key, value=G.val)
         except ObjectDoesNotExist:
-            # print(G.key + '-' + G.val + '   is not in other issue')
+            print(G.key + '-' + G.val + '   is not in other issue')
             pass
         else:
 # interrupted as the case seems to fall beyond the ambit of the FWA.
-            # print(G.key + '-' + G.val + ' is in Other')
+            print(G.key + '-' + G.val + ' is in Other')
             G.html = "other_issue"
             return render( request, 'employment/other_issue.html')
         try:
             finish=Exit.objects.get(key=G.key, value=G.val)
         except ObjectDoesNotExist:
-            # print(G.key + '-' + G.val + '   is not in exit')
+            print(G.key + '-' + G.val + '   is not in exit')
             pass
         else:
 # If G.key has reached its final value append the Pair to G.case
             G.case[G.key] = str(finish.final_value)
-            # print(G.key + '-' + G.val + '   is in exit')
+            print(G.key + '-' + G.val + '   is in exit')
 # At this point the session reached its end, and the entire transcript becomes available.
-            # print(' The session has finished, the PDF files are ready. \nThe case was:\n' + str(G.case))
+            print(' The session has finished, the PDF files are ready. \nThe case was:\n' + str(G.case))
             G.html = "exit"
             return render( request, 'employment/exit.html' )
 
         try:
             comment = Comment.objects.get( key=G.key, value=G.val )
         except ObjectDoesNotExist:
-            # print(G.key + '-' + G.val + '   is not in comment')
+            print(G.key + '-' + G.val + '   is not in comment')
             pass
         else:
-            # print(G.key + '-' + G.val + ' is in Comment')
+            print(G.key + '-' + G.val + ' is in Comment')
             if G.tense == 'past':
                 comment_text = comment.comment_text_past
             else:
@@ -291,12 +291,12 @@ def interview(request):
         try:
             question=Question.objects.get(key=G.key, value=G.val)
         except ObjectDoesNotExist:
-            # print(G.key + '-' + G.val + '   is not in question')
+            print(G.key + '-' + G.val + '   is not in question')
             pass
         else:
 ## As the class Choice returns the id in string, i.e. query set, they need to be
 ## converted to int and put into a list to iterate
-            # print('The Pair   ' + G.key + ' ' + G.val + '   points to a question')
+            print('The Pair   ' + G.key + ' ' + G.val + '   points to a question')
             G.choice_ids = []
             G.choices = []
             choice_id_list = Choice.objects.filter(key=G.key, value=G.val)
@@ -323,27 +323,27 @@ def interview(request):
         try:
             direction=Direction.objects.get(key=G.key, value=G.val)
         except ObjectDoesNotExist:
-            # print(G.key + '-' + G.val + '  is not in Direction')
+            print(G.key + '-' + G.val + '  is not in Direction')
             reference=Reference.objects.get(key=G.key, value=G.val)
-            # print(G.key + '-' + G.val + '   is in Reference')
+            print(G.key + '-' + G.val + '   is in Reference')
 # the Pair must be in Reference, i.e. G.key has reached its final value
             G.case[G.key] = str(reference.final_value)
-            # print('Pair is added to Case:')
-            # print(G.case)
+            print('Pair is added to Case:')
+            print(G.case)
 # append the Pair to G.case pop "old" Pair from the G.resolve.
-            # print(G.key + '   final_value: ' + str(reference.final_value))
+            print(G.key + '   final_value: ' + str(reference.final_value))
             pair = G.resolve.pop()
             G.key = pair[0]
             G.val = pair[1]
-            # print(G.key + '-' + G.val + '   popped from resolve')
+            print(G.key + '-' + G.val + '   popped from resolve')
             new_pair = True
         else:
 # If the Pair is in Direction, i.e. G.key has reached its final value
 # and a new Key is to be established append the Pair to G.case
             G.case[G.key] = str(direction.final_value)
-            # print('Case is appended: ' + str(G.case))
+            print('Case is appended: ' + str(G.case))
 # and establish a new Key with default value.
-            # print(G.key + '-' + G.val + '   is in Direction. The new Key: ' + str(direction.new_key))
+            print(G.key + '-' + G.val + '   is in Direction. The new Key: ' + str(direction.new_key))
             G.key = str(direction.new_key)
             G.val='(default)'
             new_pair = True
@@ -363,7 +363,7 @@ def create_output(request):
             try:
                 law = Law.objects.get(key=k, final_value=v)
             except ObjectDoesNotExist:
-                # print(k + '-' + v + '   is not in Law')
+                print(k + '-' + v + '   is not in Law')
                 pass
             else:
                 r = str(law.relevant_law)
@@ -376,14 +376,14 @@ def create_output(request):
         for r in relevant:
             relevant_pdf = open( RELEVANT_LAW + r + ".pdf", "rb" )
             merger.append(relevant_pdf)
-            # print(r + '.pdf has been uppended to relevant output')
+            print(r + '.pdf has been uppended to relevant output')
 # Create a file-like buffer to receive PDF data.
         relevant_buffer = io.BytesIO()
         relevant_buffer.flush()
-        # print('relevant_buffer assigned and flushed')
+        print('relevant_buffer assigned and flushed')
 # Write the merged pdf file into an output file-like object
         merger.write(relevant_buffer)
-        # print('relevant law output file created')
+        print('relevant law output file created')
         relevant_buffer.seek(0)
         return FileResponse(relevant_buffer, as_attachment=True, filename='Relevant law.pdf')            
 
@@ -393,7 +393,7 @@ def create_output(request):
             try:
                 law = Law.objects.get(key=k, final_value=v)
             except ObjectDoesNotExist:
-                # print(k + '-' + v + '   is not in Law')
+                print(k + '-' + v + '   is not in Law')
                 pass
             else:
                 f = str(law.full_section)
@@ -406,14 +406,14 @@ def create_output(request):
         for f in full:
             full_pdf = open( FULL_SECTIONS + f + ".pdf", "rb" )
             merger.append(full_pdf)
-            # print(f + '.pdf has been uppended to full section output')
+            print(f + '.pdf has been uppended to full section output')
 # Create a file-like buffer to receive PDF data.
         full_buffer = io.BytesIO()
         full_buffer.flush()
-        # print('full section buffer assigned and flushed')
+        print('full section buffer assigned and flushed')
 # Write the merged pdf file into an output file-like object
         merger.write(full_buffer)
-        # print('full sections output file created')
+        print('full sections output file created')
         full_buffer.seek(0)
         return FileResponse(full_buffer, as_attachment=True, filename='Full sections.pdf')            
 
